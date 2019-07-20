bb32ad368a1c0c581d0ae58a49c1fd56e443102a
Anton Shubin <2spy4x@gmail.com>
2019-07-20 11:22:24 +0300

    fix(apps/firebase-functions)#26: fix code to ensure lint and build passes
    Closes #26

--- apps/firebase-functions/src/main.ts
+++ apps/firebase-functions/src/main.ts
@@ -3,11 +3,28 @@ import { createUser, User } from '@afs/types';
 import { Request, Response } from 'express';
 
 /**
- * Firebase Function that handles HTTPS requests to "/api"
+ * Express.Query with name of user for creation
  */
-export const api = functions.https.onRequest(
-  (request: Request, response: Response): void => {
-    const user: User = createUser({});
-    response.json(user);
-  }
-);
+interface QueryWithName {
+  /**
+   * User name
+   */
+  name: string;
+}
+
+/**
+ * Express handler for requests to "/api"
+ * @param req Express.Request
+ * @param res Express.Response
+ */
+const httpsHandler = (req: Request, res: Response): void => {
+  const user: User = createUser({
+    name: (req.query as QueryWithName).name || 'Anton'
+  });
+  res.json(user);
+};
+
+/**
+ * Firebase Function for HTTPS onRequest
+ */
+export const api = functions.https.onRequest(httpsHandler);
