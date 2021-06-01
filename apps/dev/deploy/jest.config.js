3de38f2197a376c00456b11cbddb656da52f518d
Anton Shubin <2spy4x@gmail.com>
2021-06-01 23:50:45 +0300

    should work - try #1

--- apps/dev/deploy/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: '../../../jest.preset.js',
-  coverageDirectory: '../../../coverage/apps/dev/deploy',
-  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
-  displayName: 'dev-deploy',
-};
--- libs/dev/utils/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/exec';
-export * from './lib/manageParam';
-export * from './lib/getAffectedApps';
-export * from './lib/testing/environmentVariables.types';
