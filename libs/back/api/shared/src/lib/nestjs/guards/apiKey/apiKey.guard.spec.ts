600422081d75ef18874e622f8d7be7f6d0e5ab78
Anton Shubin <2spy4x@gmail.com>
2022-11-30 13:00:51 +0800

    feat(back-api-shared): add support of getting ApiKey from a request header in ApiKeyGuard

--- libs/back/api/shared/src/lib/nestjs/guards/apiKey/apiKey.guard.spec.ts
+++ libs/back/api/shared/src/lib/nestjs/guards/apiKey/apiKey.guard.spec.ts
@@ -4,34 +4,59 @@ describe(ApiKeyGuard.name, () => {
   // region SETUP
   const reflector = { get: jest.fn() };
   const guard = new ApiKeyGuard(reflector as any);
-  const getReq = jest.fn();
+  const requestMock = {
+    query: {},
+    header: jest.fn(),
+  };
   const context = {
     switchToHttp: () => ({
-      getRequest: getReq,
+      getRequest: () => requestMock,
     }),
     getHandler: jest.fn(),
   } as any;
   beforeEach(() => {
-    getReq.mockReset();
     reflector.get.mockReset();
   });
   // endregion
-  it('returns false when API Key is not provided', () => {
-    getReq.mockReturnValue({ query: {} });
-    expect(guard.canActivate(context)).toBe(false);
-  });
 
-  it('returns false when provided API Key does not equal true value', () => {
-    const providedValue = 'fake value';
+  it('returns false when API Key is not provided in query or header', () => {
     const trueValue = 'true value';
-    getReq.mockReturnValue({ query: { [API_KEY_QUERY_SEGMENT_NAME]: providedValue } });
+    requestMock.query = {};
+    requestMock.header = jest.fn();
     reflector.get.mockReturnValue(trueValue);
     expect(guard.canActivate(context)).toBe(false);
   });
 
-  it('returns true when provided API Key equals true value', () => {
+  it('returns false when provided API Key in query does not equal true value', () => {
+    const providedValue = 'fake value';
     const trueValue = 'true value';
-    getReq.mockReturnValue({ query: { [API_KEY_QUERY_SEGMENT_NAME]: trueValue } });
+    requestMock.query = { [API_KEY_QUERY_SEGMENT_NAME]: providedValue };
+    requestMock.header = jest.fn();
+    reflector.get.mockReturnValue(trueValue);
+    expect(guard.canActivate(context)).toBe(false);
+  });
+
+  it('returns false when provided API Key in header does not equal true value', () => {
+    const providedValue = 'fake value';
+    const trueValue = 'true value';
+    requestMock.query = {};
+    requestMock.header = jest.fn(() => providedValue);
+    reflector.get.mockReturnValue(trueValue);
+    expect(guard.canActivate(context)).toBe(false);
+  });
+
+  it('returns true when provided API Key in query equals true value', () => {
+    const trueValue = 'true value';
+    requestMock.query = { [API_KEY_QUERY_SEGMENT_NAME]: trueValue };
+    requestMock.header = jest.fn();
+    reflector.get.mockReturnValue(trueValue);
+    expect(guard.canActivate(context)).toBe(true);
+  });
+
+  it('returns true when provided API Key in header equals true value', () => {
+    const trueValue = 'true value';
+    requestMock.query = {};
+    requestMock.header = jest.fn(() => trueValue);
     reflector.get.mockReturnValue(trueValue);
     expect(guard.canActivate(context)).toBe(true);
   });
--- libs/back/api/shared/src/lib/nestjs/guards/apiKey/apiKey.guard.ts
+++ libs/back/api/shared/src/lib/nestjs/guards/apiKey/apiKey.guard.ts
@@ -1,6 +1,7 @@
 import { CanActivate, CustomDecorator, ExecutionContext, Injectable, SetMetadata } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
+import { LogService } from '../../../services';
 
 export const API_KEY_QUERY_SEGMENT_NAME = 'API_KEY';
 export const API_KEY_GUARD_TRUE_VALUE_TOKEN = 'API_KEY_GUARD_TRUE_VALUE_TOKEN';
@@ -11,15 +12,22 @@ export const ApiKeyGuardSetTrueValue = (value: TrueValue): CustomDecorator =>
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
+  private readonly logService = new LogService(ApiKeyGuard.name);
+
   constructor(private readonly reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const request: Request = context.switchToHttp().getRequest();
-    const apiKey = request.query[API_KEY_QUERY_SEGMENT_NAME];
-    if (!apiKey) {
-      return false;
-    }
-    const trueValue = this.reflector.get<TrueValue>(API_KEY_GUARD_TRUE_VALUE_TOKEN, context.getHandler());
-    return apiKey === trueValue;
+    return this.logService.trackSegmentSync(this.canActivate.name, logSegment => {
+      const request: Request = context.switchToHttp().getRequest();
+      const apiKey = request.query[API_KEY_QUERY_SEGMENT_NAME] ?? request.header(API_KEY_QUERY_SEGMENT_NAME);
+      const trueValue = this.reflector.get<TrueValue>(API_KEY_GUARD_TRUE_VALUE_TOKEN, context.getHandler());
+      const result = apiKey === trueValue;
+      logSegment.log(`Result`, {
+        url: request.url,
+        apiKey,
+        result,
+      });
+      return result;
+    });
   }
 }
